4391817eb583ed84ca3d5daed4b528f51cf5c081
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-23 11:06:53 +0500

    Переопределил имена полей моделей и таблиц, добавил докстринги

--- ep_crm/ep_crm1/models.py
+++ ep_crm/ep_crm1/models.py
@@ -9,6 +9,7 @@
 
 from django.db import models
 
+
 # Create your models here.
 
 class Client(models.Model):
@@ -17,11 +18,21 @@ class Client(models.Model):
     Args:
         models (django.db.models.Model): Базовый класс для всех моделей в Django
     """
-    name = models.CharField(max_length=100)
-    email = models.EmailField()
-    phone = models.CharField(max_length=20)
-    address = models.TimeField()
+    name = models.CharField(max_length=100, verbose_name="Имя (название)")
+    email = models.EmailField(verbose_name="Электронная почта")
+    phone = models.CharField(max_length=20, verbose_name="Телефон")
+    address = models.TextField(verbose_name="Адрес")
 
+    # objects = models.Manager()
+
+    def __str__(self):
+        return f"{self.name} | {self.email}"
+
+    class Meta:
+        """Изменяю название для отображения в админке
+        """
+        verbose_name="Клиент"
+        verbose_name_plural="Клиенты"
 
 class Deal(models.Model):
     """Модель для хранения данных о сделках
@@ -29,10 +40,22 @@ class Deal(models.Model):
     Args:
         models (django.db.models.Model): Базовый класс для всех моделей в Django
     """
-    client = models.ForeignKey(Client, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    status = models.CharField(max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True)
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, verbose_name="Клиент")
+    amount = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Сумма")
+    status = models.CharField(max_length=50, verbose_name="Статус")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+
+
+    def __str__(self):
+        """переопределяю название модели"""
+        return f"{self.client} | {self.amount}"
+
+
+    class Meta:
+        """Переопределяю имя модели в админке
+        """
+        verbose_name="Сделка"
+        verbose_name_plural="Сделки"
 
 
 class Task(models.Model):
@@ -41,7 +64,18 @@ class Task(models.Model):
     Args:
         models (django.db.models.Model): Базовый класс для всех моделей в Django
     """
-    deal = models.ForeignKey(Deal, on_delete=models.CASCADE)
-    description = models.TextField()
-    due_time = models.DateTimeField()
-    completed = models.BooleanField(default=False)
+    deal = models.ForeignKey(Deal, on_delete=models.CASCADE, verbose_name="Сделка")
+    description = models.TextField(verbose_name="Описание")
+    due_time = models.DateTimeField(verbose_name="Исполнить до")
+    completed = models.BooleanField(default=False, verbose_name="Исполнено")
+
+
+    def __str__(self):
+        """Переопределяю имя модели"""
+        return f"{self.deal} | {self.due_time}"
+
+
+    class Meta:
+        """Переопределяю имя модели в админке"""
+        verbose_name="Задача"
+        verbose_name_plural="Задачи"
