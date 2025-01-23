9d5398f54a79686bb1a86ff739a111525a73d8fd
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-23 11:05:34 +0500

    Добавил докстринги

--- ep_crm/ep_crm1/apps.py
+++ ep_crm/ep_crm1/apps.py
@@ -1,6 +1,9 @@
+"""Файл для настройки приложений
+"""
 from django.apps import AppConfig
 
 
 class EpCrm1Config(AppConfig):
+    """настройки приложения ep_crm1"""
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ep_crm1'
