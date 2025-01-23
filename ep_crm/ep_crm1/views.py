2ccc37d9f906910570e3b69d353a36c3ba08586c
Eugenii B. Petrov <127658418+EPoY74@users.noreply.github.com>
2025-01-23 13:14:21 +0500

    Добавил представления для данных по списку клиентов и по конкретному клиенту

--- ep_crm/ep_crm1/views.py
+++ ep_crm/ep_crm1/views.py
@@ -1,18 +1,19 @@
 """Создаю формы и представления для данных"""
 # Create your views here.
 from django.shortcuts import render, get_object_or_404
-from .models import Client, Task, Deal
+from ep_crm1.models import Client  #, Task, Deal
 # from .forms import ClientForm, DealForm, TaskForm
 
 def clients_list(request):
     """вывожу список клиентов"""
     clients = Client.objects.all()
-    return render(request, 'crm/clients_list.html',{'clients' : clients})
+    return render(request, 'clients_list.html', {'clients' : clients})
+
 
 def client_detail(request, pk):
     """Вывожу детали по клиенту"""
     client=get_object_or_404(Client,pk=pk)
-    return render(request, 'crm/client_detail.htmls',{'client' : client})
+    return render(request, 'client_detail.html',{'client' : client})
 
 
 
